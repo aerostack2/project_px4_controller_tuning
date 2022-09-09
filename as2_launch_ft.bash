ed22e4b79d7dc16498a27dbb1e5cd7144aba82c8
RPS98 <rafa.perez.s98@gmail.com>
2022-09-09 09:22:07 +0200

    Update configs

--- as2_launch_ft.bash
+++ as2_launch_ft.bash
@@ -15,33 +15,33 @@ source ./launch_tools.bash
 
 new_session $drone_namespace
 
-new_window 'controller_manager' "ros2 launch controller_manager controller_manager_launch.py \
-    drone_id:=$drone_namespace \
-    use_bypass:=true \
-    config:=config/DF/controller.yaml \
-    use_sim_time:=true"
+# new_window 'controller_manager' "ros2 launch controller_manager controller_manager_launch.py \
+#     drone_id:=$drone_namespace \
+#     use_bypass:=true \
+#     config:=config/DF/controller.yaml \
+#     use_sim_time:=true"
 
 new_window 'RTPS interface' "micrortps_agent -t UDP -n $drone_namespace"
 
-new_window 'pixhawk interface' "ros2 launch pixhawk_platform pixhawk_platform_launch.py \
-    drone_id:=$drone_namespace \
-    config:=config/DF/platform_default.yaml \
-    simulation_mode:=true \
-    use_sim_time:=true"
+# new_window 'pixhawk interface' "ros2 launch pixhawk_platform pixhawk_platform_launch.py \
+#     drone_id:=$drone_namespace \
+#     config:=config/DF/platform_default.yaml \
+#     simulation_mode:=true \
+#     use_sim_time:=true"
 
-new_window 'state_estimator' "ros2 launch basic_state_estimator basic_state_estimator_launch.py \
-    drone_id:=$drone_namespace \
-    odom_only:=true \
-    base_frame:='\"\"'  \
-    use_sim_time:=true" 
+# new_window 'state_estimator' "ros2 launch basic_state_estimator basic_state_estimator_launch.py \
+#     drone_id:=$drone_namespace \
+#     odom_only:=true \
+#     base_frame:='\"\"'  \
+#     use_sim_time:=true" 
 
-new_window 'basic_behaviours' "ros2 launch as2_basic_behaviours all_basic_behaviours_launch.py \
-    drone_id:=$drone_namespace \
-    config_follow_path:=config/DF/follow_path_behaviour.yaml \
-    config_takeoff:=config/DF/takeoff_behaviour.yaml \
-    config_land:=config/DF/land_behaviour.yaml \
-    config_goto:=config/DF/goto_behaviour.yaml \
-    use_sim_time:=true"
+# new_window 'basic_behaviours' "ros2 launch as2_basic_behaviours all_basic_behaviours_launch.py \
+#     drone_id:=$drone_namespace \
+#     config_follow_path:=config/DF/follow_path_behaviour.yaml \
+#     config_takeoff:=config/DF/takeoff_behaviour.yaml \
+#     config_land:=config/DF/land_behaviour.yaml \
+#     config_goto:=config/DF/goto_behaviour.yaml \
+#     use_sim_time:=true"
 
-new_window 'trajectory_generator' "ros2 launch trajectory_generator trajectory_generator_launch.py  \
-    drone_id:=$drone_namespace "
+# new_window 'trajectory_generator' "ros2 launch trajectory_generator trajectory_generator_launch.py  \
+#     drone_id:=$drone_namespace "
